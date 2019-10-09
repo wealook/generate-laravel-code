67d282f20d660df58ec67b34d45e5b7ea46f3889
cuibo <cuibo@cuibo.com>
2019-10-09 15:52:16 +0800

    view fix

--- tests/TestModelSR.php
+++ tests/TestModelSR.php
@@ -12,10 +12,6 @@ class TestModelSR extends TestCase
         $this->artisan('produce:model_sr Admin\\\Config --table=admin_config   --route=web');
         $controller = $this->app->make('App\Http\Controllers\Admin\ConfigController');
         $this->get('/admin/config')->assertOk();
-
-        $this->artisan('produce:model_sr Admin\\\Config --table=admin_config   --route=web --view=config');
-        $controller = $this->app->make('App\Http\Controllers\Admin\ConfigController');
-        $this->get('/admin/config')->assertOk();
     }
 
 }
